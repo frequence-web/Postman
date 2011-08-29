6bc7ff2fbda85e3adb6c9933b10851496899ab47
yohang <yohan@giarelli.org>
2011-08-29 20:58:12 +0200

    Refactored some classes

--- src/Postman/Request/RequestHandler.php
+++ src/Postman/Request/RequestHandler.php
@@ -14,23 +14,23 @@ use \Postman\Request\Event\GetResponseEvent;
 
 class RequestHandler extends ContainerAware implements RequestHandlerInterface
 {
-  public function handle(RequestEvent $event)
-  {
-    $requestHeaders = array();
-    while (($line = fgets($event->getConnection(), 1024)) != "\r\n") {
-        $requestHeaders[] = $line;
+    public function handle(RequestEvent $event)
+    {
+        $requestHeaders = array();
+        while (($line = fgets($event->getConnection(), 1024)) != "\r\n") {
+            $requestHeaders[] = $line;
+        }
+
+        $request = Request::createFromHttp($requestHeaders, $this->getParameter('port'));
+        $request->server->set('REMOTE_HOST', stream_socket_get_name($event->getConnection(), true));
+        $request->server->set('REMOTE_ADDR', gethostbyname(stream_socket_get_name($event->getConnection(), true)));
+        $request->server->set('DOCUMENT_ROOT', $this->getParameter('basedir'));
+
+        $this->get('logger')->debug('Sending get_response event');
+        $getResponseEvent = new GetResponseEvent($request);
+        $this->get('event_dispatcher')->dispatch('postman.get_response', $getResponseEvent);
+
+        $this->get('logger')->info('Sending response');
+        fwrite($event->getConnection(), (string)$getResponseEvent->getResponse(), strlen((string)$getResponseEvent->getResponse()));
     }
-
-    $request = Request::createFromHttp($requestHeaders, $this->getParameter('port'));
-    $request->server->set('REMOTE_HOST', stream_socket_get_name($event->getConnection(), true));
-    $request->server->set('REMOTE_ADDR', gethostbyname(stream_socket_get_name($event->getConnection(), true)));
-    $request->server->set('DOCUMENT_ROOT', $this->getParameter('basedir'));
-
-    $this->get('logger')->debug('Sending get_response event');
-    $getResponseEvent = new GetResponseEvent($request);
-    $this->get('event_dispatcher')->dispatch('postman.get_response', $getResponseEvent);
-
-    $this->get('logger')->info('Sending response');
-    fwrite($event->getConnection(), (string)$getResponseEvent->getResponse(), strlen((string)$getResponseEvent->getResponse()));
-  }
 }
